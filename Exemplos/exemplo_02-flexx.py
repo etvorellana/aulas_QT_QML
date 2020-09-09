bb13c7e41ae482938cd140d440a6b67bbf1eafe0
etvorellana <evalero@uesc.br>
2020-09-09 10:55:13 -0300

    Novos Exemplos de GUIs em Python

--- Exemplos/exemplo_02-flexx.py
+++ Exemplos/exemplo_02-flexx.py
@@ -3,10 +3,14 @@ from flexx import flx
 class Example(flx.Widget):
 
     def init(self):
-        flx.Button(text='hello')
-        flx.Button(text='world')
+        self.set_title("A simple GUI")
+        with flx.VBox():
+            flx.Label(text="This is our first GUI!")
+            flx.Button(text='Greet')
+            flx.Button(text='Close')
+
 
 app = flx.App(Example)
-#app.launch('app')  # to run as a desktop app
-app.launch('browser')  # to open in the browser
+app.launch('app')  # to run as a desktop app
+#app.launch('browser')  # to open in the browser
 flx.run()  # mainloop will exit when the app is closed
